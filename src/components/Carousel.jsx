8b1f465142e7ccacf4fc2279433aefd94ec4c030
aniketgoyal51 <aniketgoyal791@gmail.com>
2023-11-23 11:36:10 +0530

    .

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,48 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
+    constructor() {
+        super()
+        
+        this.state = {
+            count: 0
+        }
+  }
   
+  increase=()=>{
+    if(this.state.count<2){
+        this.setState({count: this.state.count+1})
+    }else if(this.state.count>=2){
+        this.setState({count:0})
+    }
+}
+
+decrease=()=>{
+    if(this.state.count>0){
+        this.setState({count: this.state.count-1})
+    }else if(this.state.count<=0){
+        this.setState({count:2})
+    }
+}
+
+render(){
+    return(
+        <>
+            <div className="Container">
+                <div className="leftbutton" onClick={this.decrease}>
+                    <ArrowBackIosIcon/>
+                </div>
+                <img src={images[this.state.count].img} alt="" />
+                <div className="rightbutton" onClick={this.increase}>
+                    <ArrowForwardIosIcon/>
+                </div>
+            </div>
+            <div className="caption">
+                <h3>{images[this.state.count].subtitle}</h3>
+            </div>
+        </>
+    )
+  }
 }
 
 export default Carousel;
